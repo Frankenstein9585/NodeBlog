6afe510a4161083b5fbf8f82c4e2aa57bd5ecd6d
Donald Peters <donny9585@gmail.com>
2024-09-30 15:15:24 +0100

    Connected database and added pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,34 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    const locals = {
-        title: "NodeJS Blog",
-        description: "Simple Blog created with NodeJS, Express & MongoDB."
-    };
+router.get('/', async (req, res) => {
+    try {
+        const locals = {
+            title: "NodeJS Blog",
+            description: "Simple Blog created with NodeJS, Express & MongoDB."
+        };
 
-    res.render('index', { locals });
+        const perPage = 10;
+        let page = req.query.page || 1;
+
+        const posts = await Post.aggregate([ { $sort: { createdAt: -1} } ])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasnextPage = nextPage <= Math.ceil(count / perPage);
+        res.render('index', {
+            locals,
+            posts,
+            current: page,
+            nextPage: hasnextPage ? nextPage : null
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/about', (req, res) => {
